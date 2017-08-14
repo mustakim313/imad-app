2dcebae8d477a461736f4edc14df5324677df606
mustakim313 <30827293+mustakim313@users.noreply.github.com>
2017-08-14 12:34:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artical-one', finction (req,res){
+app.get('/artical-one', function (req,res){
 });
-    rse.send('artical one is requested and will be served here')
+    res.send('artical one is requested and will be served here')
 }
 app.get('/artical-two', finction (req,res){
 });
-    rse.send('artical two is requested and will be served here')
+    res.send('artical two is requested and will be served here')
 }
 app.get('/artical-three', finction (req,res){
 });
-    rse.send('artical three is requested and will be served here')
+    res.send('artical three is requested and will be served here')
 }
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
