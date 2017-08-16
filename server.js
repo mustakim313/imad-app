38463818340edca494fb5b5c2551b2c2d481e390
mustakim313 <30827293+mustakim313@users.noreply.github.com>
2017-08-16 10:59:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,61 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+  
+   var articleone=
+   {
+       title: 'Article one | mustakim kureshi',
+       heading: 'Article one',
+       date: 'Aug 16 2017',
+       content:`  <p>
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first articlhis is the content for my first article. This is the content for my first article This is the content for my first articleThis is the content for my first article.This is the content for my first article.  
+    </p>
+    <p>
+           2nd paragrapfh This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first articlhis is the content for my first article. This is the content for my first article This is the content for my first articleThis is the content for my first article.This is the content for my first article.  
+    </p>
+    <p>
+            3rd paragraph This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first articlhis is the content for my first article. This is the content for my first article This is the content for my first articleThis is the content for my first article.This is the content for my first article.  
+    </p>`
+    };
+    
+   function createTemplate(data){
+       var titile = data.title;
+       var date = data.date;
+       var heading = data.heading;
+       var conten = data.cntent;
+       
+       
+    var htmlTemlate =`
+        <html>
+    <head>
+        <title>
+          ${title}
+        </title>
+      <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+        <a href="/">HOME</a>
+        </div>
+       <hr>
+        <h3>
+       ${heading}
+       </h3>
+       <div>
+       ${date}
+       </div>f
+       ${content}
+       </div>
+    </body>
+        
+    </html>
+  `;
+  return htmlTemplet;
+   }
 app.get('/artical-one', function (req,res){
 
-  res.sendFile(path.join(__dirname,'mustakim.html'));
+  res.send(createTemplet(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
