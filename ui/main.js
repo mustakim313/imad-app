ab7d6f4087a2a1eb0839e0776ce40c4cec50d082
mustakim313 <30827293+mustakim313@users.noreply.github.com>
2017-08-16 16:12:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ function moveRight(){
     marginLeft= marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
-img.onclick= function(){
-    var interval=setInterval(moveRight,0);
+    img.onclick= function(){
+    var interval = setInterval(moveRight,100);
 };
