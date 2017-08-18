6b08df92a2310d0879d90b0b88f5a99f2befba22
mustakim313 <30827293+mustakim313@users.noreply.github.com>
2017-08-18 10:54:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,20 @@
 var button= document.getElementById('counter');
 var counter=0;
 button.onclick= function (){
-
-counter = counter+1;
-var span = document.getElementById('count');
+    var request = new XMLHttpRequest();
+    request.onreadystatechange =function(){
+        if(request.readystate===XmLhttpRequest.DONE){
+            if(request.status==200){
+                var counter =request.ResponseText;
+                var span = document.getElementById('count');
 span.innerHTML= counter.toString();
+            }
+        }
+    }
+
+;
+request.open('GET','http://mustakimkureshi313.imad.hasura-app.io/countr',true);
+request.send(null);
+
 };
 
